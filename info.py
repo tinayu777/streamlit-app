88c05347f00cbbf5e00510df2970ecb4a6dead14
tinayu777 <kristinayu777@gmail.com>
2024-11-26 19:09:28 -0500

    Initial commit

---
+++ info.py
@@ -0,0 +1,73 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/tina-profile.jpg"
+about_me = "I'm Kristina Yu and I am an Industrial Design student at Georgia Tech. "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/kristina-yu-7408372bb/"
+my_github_url = "https://github.com/tinayu777"
+my_email_address = "kristinayu777@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Industrial Design',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': '2027',
+    'GPA': '3.5'
+}
+course_data = {
+    "code":["ID 1011", "1D 1401", "ID 1418", "ID 1101"], 
+    "names":["Industrial Design Fundamentals", "Intro to Graphic Communication", "Intro to Sketching and Modeling", "Intro to Inudstrial Design"], 
+    "semester_taken":["3rd", "3rd", "3rd", "3rd"],
+    "skills":["The Design Process from beginning to end", "How to use raphic design effectively", "How to create models from a variety of materials", "Learned about jobs and real-life opportunities"],
+    }
+experience_data = {
+    "Supplemental Instruction Leader for Calculus 1 at Kennesaw State University": (["- Lead and facilitate large group study sessions",
+                                                                          "- Collaborate with my team", "- Create lesson plans"],"Images/cook.jpg"),
+    "Intern at The Shine Community":(["- Design and create products",
+                                                           "- Teach coding classes to young adults with developmental disabillities"],"Images/cleaner.jpg"),
+    "Hostess at Arita Ramen and Sushi Bar":(["- Greet guests, assign tables to servers, take and pack to-go orders"],"Images/jelly.jpg")
+
+}
+
+projects_data = {
+    "Regeneron International Science and Engineering Fair 2021 Finalist": "Made a device that can help predict wildfires",
+}
+
+programming_data = {
+    "Python": 90,
+    "Java": 70,
+    "C": 40,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+spoken_icons = {"Mandarin": "🇨🇳",
+    "English": "🇬🇧",
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Mandarin": "Fluent",
+}
+leadership_data = {
+    "Chinese Club": (["- Creative Director"],"Images/puff.jpg"),
+
+}
+activity_data={
+    "IDSA": ["- Industrial Design Society of America"]
+}
